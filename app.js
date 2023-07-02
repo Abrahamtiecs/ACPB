b71515c05c077c398c44696466a489476fcf5e1d
Abrahamtiecs <128197045+Abrahamtiecs@users.noreply.github.com>
2023-07-02 08:06:09 +0000

    final

---
+++ app.js
@@ -0,0 +1,34 @@
+var myDiv = document.getElementById("myDiv");
+
+myDiv.addEventListener("click", function() {
+  window.location.href = "esa.html";
+});
+
+function espe() {
+    window.location.href = "https://www.example.com";
+}
+
+function esmg() {
+  window.location.href = "https://www.example.com";
+}
+function escae() {
+  window.location.href = "https://www.example.com";
+}
+
+function sport() {
+  window.location.href = "https://www.example.com";
+}
+function english() {
+  window.location.href = "https://www.example.com";
+}
+
+function carriere() {
+  window.location.href = "https://www.example.com";
+}
+function final() {
+  window.location.href = "https://www.example.com";
+}
+function plus() {
+  window.location.href = "https://www.example.com";
+}
+  
